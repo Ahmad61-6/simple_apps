2a4989df031df1a907c3e76f720b006281308289
Ahmad61_6 <ahmadmizan210@gmail.com>
2023-12-26 21:07:32 +0600

    --Stateful widget --setState() --simple counter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,50 @@
-void
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Counter App",
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int countNumber = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Counter App"),
+      ),
+      body: Center(
+        child: Text("Counting Number\n ${countNumber.toString()}", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (){
+          setState(() {
+            countNumber++;
+          });
+        },
+      ),
+    );
+  }
+}
+
