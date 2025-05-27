d5f7c8d253cfdff5b9421f2c24dede5fc0d81d68
Ahmad61-6 <ahmadmizan210@gmail.com>
2025-05-27 12:10:08 +0600

    swithced

--- lib/presentation/utility/assets_path.dart
+++
@@ -1,7 +0,0 @@
-class AssetsPath {
-  static const String imagePath = 'assets/images';
-  static const String avocado = '$imagePath/avocado.png';
-  static const String banana = '$imagePath/banana.png';
-  static const String milk = '$imagePath/milk.png';
-  static const String burger = '$imagePath/burger.png';
-}
